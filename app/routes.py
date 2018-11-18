a388c0ce86ef0633144cc245ebea9b21a6574575
brett <vande060@gmail.com>
2018-11-17 23:07:14 -0800

    pilot timer

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/quick_timer')
+def quick_timer():
+    return render_template('quick_timer.html')
