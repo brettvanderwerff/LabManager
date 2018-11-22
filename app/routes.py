d5e1dd5de610746e7c87dc6a5041bb6da30dc79d
brett <vande060@gmail.com>
2018-11-22 11:24:17 -0800

    working form

--- app/routes.py
+++ app/routes.py
@@ -17,3 +17,5 @@ def set_up_timers():
 
 #ToDo just focus on making a page with grid of timers that have a title and can be run independently then focus on letting user save those timers
 #ToDo add web form above timer to record what the timer is for
+# probably need another grid within a grid for webform in timer
+# just get submit form to work first, worry about style later
