24639d0db9dd41763c8e2cad65ad47788a75c0e9
brett <vande060@gmail.com>
2018-11-25 22:36:30 -0800

    change columns to percent

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,13 @@ def quick_timer():
 def set_up_timers():
     return render_template('set_up_timers.html')
 
+@app.route('/timer_array/<number_timers>')
+def button_array(number_timers):
+    return render_template('timer_array.html')
+
 
 
 #ToDo just focus on making a page with grid of timers that have a title and can be run independently then focus on letting user save those timers
+#ToDo radio buttons to choose timer sound
+#ToDo make display flash when timer is up
+#ToDo make display count negative time after timer expires
